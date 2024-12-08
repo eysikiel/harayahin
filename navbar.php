23795ca04cf60ff03452a01476e9d0f57db86485
Mae Daniella Faith Benjamin <mdfcbenjamin@gmail.com>
2024-12-08 21:00:31 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,3 +1,8 @@
+<!--  FONT (Sono) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sono:wght@200..800&display=swap" rel="stylesheet">
+
 <div class="navbar">
     <div class="logo">
         <img src = "logo.png">
@@ -7,7 +12,7 @@
         <a href="index.php">Home</a>
         <a href="DedicateAHarana.php">Dedicate A Harana</a>
         <a href="KundimanKanto-index.php">Kundiman Kanto</a>
-        <a href ="search.php">Find Your Harana</a>
+        <a href ="FindYourHarana.php">Find Your Harana</a>
         <a href="aboutus.php">About Us</a>
     </nav>
 </div>
