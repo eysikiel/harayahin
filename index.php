bdc10f827b32a1d401595332f70ef032b5a611b4
Mae Daniella Faith Benjamin <mdfcbenjamin@gmail.com>
2024-12-08 20:58:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,18 @@
 <html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
+
+    <title>Harayahin</title>
     <link rel="stylesheet" href="index.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Qwitcher+Grypen:wght@400;700&family=Sono:wght@200..800&display=swap" rel="stylesheet">
+
+    <!-- FONTS USED -->
+				<link rel="preconnect" href="https://fonts.googleapis.com">
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+				<link href="https://fonts.googleapis.com/css2?family=Qwitcher+Grypen:wght@400;700&family=Sono:wght@200..800&display=swap" rel="stylesheet">
+
 </head>
 <body>
-    <div>
-    <?php
-     include 'navbar.php'
-     
-     ?>
-    </div>
-    <!-- <a href="navbar.html"></a> -->
-   
+    <!-- NAVIGATION BAR -->
+    <?php include 'navbar.php'
+?>
 </body>
 </html>
